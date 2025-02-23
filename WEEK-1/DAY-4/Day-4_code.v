5eae6d86aac99a95984a81884b442628b9958fa4
Prithwin14 <10j21prithwin.vp@gmail.com>
2025-02-23 21:45:13 +0530

    Create Day-4_code.v

---
+++ WEEK-1/DAY-4/Day-4_code.v
@@ -0,0 +1,107 @@
+#Vector0
+
+module top_module ( 
+    input wire [2:0] vec,
+    output wire [2:0] outv,
+    output wire o2,
+    output wire o1,
+    output wire o0  ); 
+    assign outv = vec;
+    assign o0 = vec[0];
+    assign o1 = vec[1];
+    assign o2 = vec[2];
+
+endmodule
+
+#Vector1
+
+`default_nettype none     // Disable implicit nets. Reduces some types of bugs.
+module top_module( 
+    input wire [15:0] in,
+    output wire [7:0] out_hi,
+    output wire [7:0] out_lo );
+    assign out_lo[7:0] = in[7:0];
+    assign out_hi[7:0] = in[15:8];
+
+endmodule
+
+#Vector2
+
+module top_module( 
+    input [31:0] in,
+    output [31:0] out );//
+
+    assign out = {in[7:0],in[15:8],in[23:16],in[31:24]};
+
+endmodule
+
+#Vectorgates
+
+module top_module( 
+    input [2:0] a,
+    input [2:0] b,
+    output [2:0] out_or_bitwise,
+    output out_or_logical,
+    output [5:0] out_not
+);
+    assign out_or_bitwise = a | b;
+    assign out_or_logical = a||b;
+    assign out_not = {~b, ~a};
+
+endmodule
+
+#Gates4
+
+module top_module( 
+    input [3:0] in,
+    output out_and,
+    output out_or,
+    output out_xor
+);
+    assign out_and = {in[0]&in[1]&in[2]&in[3]};
+    assign out_or = {in[0]|in[1]|in[2]|in[3]};
+    assign out_xor = {in[0]^in[1]^in[2]^in[3]};
+
+endmodule
+
+#Vector3
+
+module top_module (
+    input [4:0] a, b, c, d, e, f,
+    output [7:0] w, x, y, z );//
+
+    assign {w,x,y,z} = {a,b,c,d,e,f,1'b1,1'b1};
+
+endmodule
+
+#Vectorr
+
+module top_module( 
+    input [7:0] in,
+    output [7:0] out
+);
+    assign out[7:0] = {in[0],in[1],in[2],in[3],in[4],in[5],in[6],in[7]};
+
+endmodule
+
+#Vector4
+
+module top_module (
+    input [7:0] in,
+    output [31:0] out );//
+
+    assign out = { {24{in[7]}} , in};
+
+endmodule
+
+#Vector5
+
+module top_module (
+    input a, b, c, d, e,
+    output [24:0] out );//
+
+    // The output is XNOR of two vectors created by 
+    // concatenating and replicating the five inputs.
+    assign out =  ~{ {5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}} } ^ { 5{a,b,c,d,e} };
+
+endmodule
